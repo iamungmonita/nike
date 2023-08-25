262684f85f11bee420f0a7b7b7736923c13fb210
Ung Montia <iamungmonita@gmail.com>
2023-08-25 17:35:33 +0900

    removed trending from main index

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,22 +3,9 @@ import { Category } from "@/models/Category"
 import { Card } from "@/core"
 import { useRouter } from "next/router"
 import { AHelmet } from "@/core"
-import { getAllTrending } from "./service/trending"
+
 
 export default function Home() {
-  const [items, setItems] = useState<Category[]>([])
-
-  useEffect(() => {
-    initFunction()
-  }, [])
-
-  function initFunction() {
-    Promise.resolve((getAllTrending()).then((response) => {
-      setItems(response)
-    }))
-  }
-
-
 
 
   return (
